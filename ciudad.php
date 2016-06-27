41aa98b12f2ef859d67a11de0c3f20f2438da450
Dacosta <danny3549@hotmail.com>
2016-06-27 12:51:31 -0500

    typeahead departamentos

--- ciudad.php
+++ ciudad.php
@@ -1,21 +1,21 @@
 <?php
 	$con = mysqli_connect("localhost","root","","colombia");
-
+	$ciudad = [];
 	// Check connection
 	if (mysqli_connect_errno())
 	  {
 	  echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	  }
 
-	if ($resultado = $con->query("SELECT * FROM `departamento` WHERE `nombre` LIKE '%".$_GET['query']."%'")) {
+	if ($resultado = $con->query("SELECT * FROM `ciudad` WHERE id_departamento = '".$_GET['departamento']."' AND `nombre` LIKE '%".$_GET['query']."%'")) {
 	    /* liberar el conjunto de resultados */
 
 	    while ($row = $resultado->fetch_object()){
-        	$departamentos[] = $row;
+        	$ciudad[] = $row;
     	}
 	    $resultado->close();
 
-	    print json_encode($departamentos);
+	    print json_encode($ciudad);
 	}
 
 ?>
--- main.js
+++ main.js
@@ -21,12 +21,13 @@ jQuery(document).ready(function() {
 	});
 	jQuery('#ciudad').selectize();
 	*/
-	
-	var states = new Bloodhound({
-          datumTokenizer: function(d) { return Bloodhound.tokenizers.whitespace(d.num); },
+		var departamentoSelecionado= 0;
+		var departamentos = new Bloodhound({
+          datumTokenizer: Bloodhound.tokenizers.obj.whitespace('nombre'),
           queryTokenizer: Bloodhound.tokenizers.whitespace,
           remote: {
-                url: "departamentos.php",
+          		header: '<h3 class="league-name">Departamentos</h3>',
+                url: "departamentos.php?query=%QUERY",
                 filter: function (departamentos) {
                 	//departamentos = JSON.parse(departamentos);
                     return $.map(departamentos, function (departamento) {
@@ -37,22 +38,99 @@ jQuery(document).ready(function() {
                         };
 
                     });
-                }
+                },
+                wildcard: '%QUERY'
            },
            limit: 1000
         });
-        states.initialize();
+       	
+       	departamentos.initialize();
 
-	$('#departamentos').typeahead(null, {
-	  name: 'Departamentos',
-	  display: 'departamento',
-	  source: states,
-	  templates: {
-		suggestion: function(data){
-		      return '<p><strong>Departamento</strong> - ' + data.nombre + '</p>';
-	  }
+		var ciudad = new Bloodhound({
+          datumTokenizer: Bloodhound.tokenizers.obj.whitespace('nombre'),
+          queryTokenizer: Bloodhound.tokenizers.whitespace,
+          remote: {
+          		wildcard: '%QUERY',
+          		header: '<h3 class="league-name">ciudad</h3>',
+                url: "ciudad.php",
+                cache:false,
+                prepare: function (query, settings) {
+                	var q = "ciudad.php";
+  		            if (departamentoSelecionado) {
+		                q += '?departamento='+encodeURIComponent(departamentoSelecionado);
+		                q += '&query='+encodeURIComponent(query);
+		            }
 
-	}});
+		            return q;
+		        },
+                filter: function (ciudades) {
+                	ciudades = JSON.parse(ciudades);
+                	//departamentos = JSON.parse(departamentos);
+                    return $.map(ciudades, function (ciudad) {
 
+                        return {
+                            num: ciudad.id_ciudad,
+                            nombre: ciudad.nombre.toLowerCase()
+                        };
 
+                    });
+                }
+                
+           },
+           limit: 1000
+        });
+
+		ciudad.initialize();
+       /*
+       	var countries = new Bloodhound({
+		  datumTokenizer: Bloodhound.tokenizers.whitespace,
+		  queryTokenizer: Bloodhound.tokenizers.whitespace,
+		  // url points to a json file that contains an array of country names, see
+		  // https://github.com/twitter/typeahead.js/blob/gh-pages/data/countries.json
+		prefetch: {
+			url:'departamentos.php'
+		  }		
+		});	
+		*/
+        departamentos.initialize();
+
+		$('#departamentos .typeahead').typeahead(null, {
+			minLength: 0,
+  			highlight: true,
+  			name: 'Departamentos',
+			display: 'nombre',
+			source: departamentos.ttAdapter(),
+			templates: {
+				empty: [
+			      '<div class="empty-message">',
+			        'No se encontraron datos',
+			      '</div>'
+			    ].join('\n'),
+				suggestion: function(data){
+					return '<p><strong>' + data.nombre + '</strong></p>';
+				}
+
+			}
+		}).on('typeahead:selected', function (e, departamento) {
+		    departamentoSelecionado = departamento.num;
+		});
+
+		$('#ciudad .typeahead').typeahead(null, {
+			minLength: 0,
+  			highlight: true,
+  			name: 'Ciudad',
+			display: 'nombre',
+			source: ciudad.ttAdapter(),
+			templates: {
+				empty: [
+			      '<div class="empty-message">',
+			        'No se encontraron datos',
+			      '</div>'
+			    ].join('\n'),
+				suggestion: function(data){
+					return '<p><strong>' + data.nombre + '</strong></p>';
+				}
+
+			}
+		});
 });
